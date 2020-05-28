f253df188237f6a3cfdcd8fc688bd07783635aa2
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-05-29 02:28:53 +0800

    Validation Mode

--- src/main/java/com/jacobianmatthews/pulsarvalidator/PulsarValidator.java
+++ src/main/java/com/jacobianmatthews/pulsarvalidator/PulsarValidator.java
@@ -1,30 +1,34 @@
 package com.jacobianmatthews.pulsarvalidator;
 
+import java.io.IOException;
+
 /**
- * This program is a validator for the machine learning software PulsarClassifier. 
+ * This program is a validator for the machine learning software
+ * PulsarClassifier.
  * 
- * This program will generate a list of candidate filenames from an output file of PulsarFeatureLab.
- * It will then be possible to filter the list of known pulsars in the original dataset to the pulsars that had
- * successful feature extraction. Finally, the program can compare the list of known pulsars in the dataset
- * to the pulsars classified in the classifier, and produce comparison statistics.
+ * This program will generate a list of candidate filenames from an output file
+ * of PulsarFeatureLab. It will then be possible to filter the list of known
+ * pulsars in the original dataset to the pulsars that had successful feature
+ * extraction. Finally, the program can compare the list of known pulsars in the
+ * dataset to the pulsars classified in the classifier, and produce comparison
+ * statistics.
  * 
  * @author Jacob Ian Matthews
  * @version 1.0, 28/05/2020
  */
-public class PulsarValidator 
-{
+public class PulsarValidator {
 
     /**
      * 
      * VARIABLES
      * 
-    */
+     */
 
     // Pulsar list mode
     private static boolean list = false;
 
-    // Comparison of PulsarClassifier output and list of pulsars in dataset mode
-    private static boolean compare = false;
+    // Pulsar classification output validation mode
+    private static boolean validation = false;
 
     // String containaing the path to the PulsarFeatureLab output file
     private static String pflOutput;
@@ -38,30 +42,57 @@ public class PulsarValidator
     // String containing the path to the negative classifier output
     private static String negativeClassifier;
 
-
-    public static void main( String[] args )
+    public static void main(String[] args) throws IOException
     {
         // Get the input arguments
         getCliVariables(args);
 
         // Check to see which flag has been given
         if ( list ){
-            // Enter list mode
+            // LIST MODE CHOSEN
+            // Print a message
+            System.out.println("Pulsar list generation mode chosen.\n");
+
+            // Create a list mode instance
+            System.out.println("This feature is under development. Exiting program.");
+            System.exit(0);
 
 
-        } else if ( compare ) {
 
-            // Enter comparison mode
+        } else if ( validation ) {
 
-        } else if ( list && compare ){
+            // VALIDATION MODE CHOSEN
+            // Print a message
+            System.out.println("Pulsar classifier validation mode chosen. \n");
+
+            // Create the validation mode instance
+            ValidationMode validationMode = new ValidationMode(pulsarList, positiveClassifier, negativeClassifier);
+
+            // Get the output string
+            String output = validationMode.validate();
+
+            // Output the string and then exit the program
+            System.out.println("Pulsar classifier validated successfully.\n");
+            System.out.println(output);
+
+            System.exit(0);
+
+
+        } else if ( list && validation ){
 
             // Display error that you can only do one thing at once
+            System.out.println("'-l' and '-v' arguments entered. Please choose one mode only. \n");
 
+            // Exit the application
+            System.exit(0);
 
-        } else if ( !list && !compare ){
+        } else if ( !list && !validation ){
 
             // Display error that you need to pick a flag
+            System.out.println("Please choose a mode by using this program with a '-l' or '-v' argument.\n");
 
+            // Exit the application
+            System.exit(0);
         }
 
 
@@ -94,10 +125,10 @@ public class PulsarValidator
                 pulsarList = args[i+2];
 
             // Check for compare flag
-            } else if( argument.equals("-c") ){
+            } else if( argument.equals("-v") ){
 
                 // Set the compare mode boolean to true
-                compare = true;
+                validation = true;
 
                 // Get the list of pulsars
                 pulsarList = args[i+1];
